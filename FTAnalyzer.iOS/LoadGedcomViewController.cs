d3254f7115e18407abc992c23030deb37c34dbee
Alexander Bisset (Mac) <ambisset@btinternet.com>
2018-03-06 16:45:55 +0000

    Font displays on screen

--- FTAnalyzer.iOS/LoadGedcomViewController.cs
+++ FTAnalyzer.iOS/LoadGedcomViewController.cs
@@ -8,12 +8,19 @@ namespace FTAnalyzer.iOS
     {
         public LoadGedcomViewController (IntPtr handle) : base (handle)
         {
+        }
+
+		public override void ViewDidLoad()
+		{
+            base.ViewDidLoad();
             var height = UIScreen.MainScreen.Bounds.Height;
             var width = UIScreen.MainScreen.Bounds.Width;
-            var label = new UILabel(new CGRect(0, 0, width, height / 2));
-            label.Font = UIFont.FromName("Kunstler Script", 72.0f);
-            label.Text = "Family Tree Analyzer";
+            var label = new UILabel(new CGRect(0, 0, width, height / 2.0f))
+            {
+                Font = UIFont.FromName("Kunstler Script", height/8.0f),
+                Text = "Family Tree Analyzer",
+            };
             Add(label);
-        }
-    }
+		}
+	}
 }
