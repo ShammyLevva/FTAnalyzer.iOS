42fdf7a114e49c5fc8acba750c4300c55a5d91fe
Alexander Bisset (Mac) <ambisset@btinternet.com>
2018-03-06 12:16:57 +0000

    add icons without alpha for iOS add gedcom view controller change loadgedcom to be startup screen add font for logo text

---
+++ FTAnalyzer.iOS/LoadGedcomViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace FTAnalyzer.iOS
+{
+    public partial class LoadGedcomViewController : UIViewController
+    {
+        public LoadGedcomViewController (IntPtr handle) : base (handle)
+        {
+            
+        }
+    }
+}
