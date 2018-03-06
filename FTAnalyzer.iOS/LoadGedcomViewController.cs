d95c4f9a3a6a2e10dcbff34c3f0c1a466d20f54b
Alexander Bisset (Mac) <ambisset@btinternet.com>
2018-03-06 16:51:00 +0000

    Font sized and positioned

--- FTAnalyzer.iOS/LoadGedcomViewController.cs
+++ FTAnalyzer.iOS/LoadGedcomViewController.cs
@@ -15,7 +15,7 @@ namespace FTAnalyzer.iOS
             base.ViewDidLoad();
             var height = UIScreen.MainScreen.Bounds.Height;
             var width = UIScreen.MainScreen.Bounds.Width;
-            var label = new UILabel(new CGRect(0, 0, width, height / 2.0f))
+            var label = new UILabel(new CGRect(width / 5.0f, 0, width, height / 2.0f))
             {
                 Font = UIFont.FromName("Kunstler Script", height/8.0f),
                 Text = "Family Tree Analyzer",
